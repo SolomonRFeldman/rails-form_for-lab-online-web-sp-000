191554071d6c48d153d7046cda5330def7d73c3d
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-05 22:15:37 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,27 @@
 class SchoolClassesController
   
+  def index
+    
+  end
   
+  def show
+    
+  end
+
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
   
 end
